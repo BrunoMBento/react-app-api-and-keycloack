5cdca98baf5c311c9c17c54c3d07a0a4fbf6b5d4
p_7530 <bruno.bento@camara.leg.br>
2025-08-18 19:49:52 -0300

    feat: adiciona os primeiros componentes do shadcnui

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
+import { Button } from "./components/ui/button";
 
 export function App() {
  return (
-    <h1>Hello World</h1>
+    <div className="p-6 max-w-4xl mx-auto">
+        <div className="border rounded">
+            <Button>Oi</Button>
+        </div>
+        <h1>Hello World</h1>
+    </div>
  )
 }
